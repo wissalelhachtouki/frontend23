7df424498e97048701f7712e0e7474e04d9a21d7
belkas anass <anassbelkas@gmail.com>
2021-05-09 18:13:52 +0000

    anass commited

--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -2,8 +2,6 @@ import { DropDown } from "../components";
 import { Parallax } from "../components";
 import { VPopover } from "v-tooltip";
 
-//import { FormGroupInput, Card, DropDown2, Button } from "../components/components2/index";
-
 /**
  * You can register global components here and use them as a plugin in your main Vue instance
  */
@@ -13,10 +11,6 @@ const GlobalComponents = {
     Vue.component("drop-down", DropDown);
     Vue.component(Parallax.name, Parallax);
     Vue.component("v-popover", VPopover);
-    //Vue.component("fg-input", FormGroupInput);
-    //Vue.component("drop-down", DropDown2);
-    //Vue.component("card", Card);
-    //Vue.component("p-button", Button);
   }
 };
 
