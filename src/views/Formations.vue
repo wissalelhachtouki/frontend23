8a4ec36f2f2140f604225ba6f55dcc12ead6f27f
belkas anass <anassbelkas@gmail.com>
2021-05-11 14:14:43 +0000

    anass commitedd

--- src/views/Formations.vue
+++ src/views/Formations.vue
@@ -2,18 +2,137 @@
   <div class="formations">
     <MainSidebar/>
     <div class="home_content">
-      <h1>this is liste des formations</h1>
+      <div class="d-flex justify-content-center width">
+        <div class="border  p-2 mt-5 w-100 background-color">
+      <!-- Main content -->
+          <div class="card" style="background-color: #caf7e3 ; border-radius: 15px ; border-color: black  ">
+            <div class="card-header" style="border-radius: 15px"><strong>Liste des Formation</strong></div>
+            <div class="card-body">
+
+              <table class="table table-bordered table-dark table-hover table-striped">
+                <thead>
+
+                <th>Ref</th>
+                <th>Titre</th>
+                <th>Nombre de jours</th>
+                <th>Tarifs par jours</th>
+                <th>Nombre de participants</th>
+                <th>Actions</th>
+
+                </thead>
+                <tbody>
+                <tr>
+
+                  <td class="font-weight-bold">#</td>
+                  <td class="font-weight-bold">test2</td>
+                  <td class="font-weight-bold">test3</td>
+                  <td class="font-weight-bold">test4 DH</td>
+                  <td class="font-weight-bold">test5</td>
+                  <td>
+                    <span><i class="bx bxs-plus-square btn btn-info" style="margin: 0 5px"></i></span>
+                    <span><i class="fa fa-edit btn btn-warning" style="margin: 0 5px"></i></span>
+                    <span><i class="bx bxs-trash btn btn-danger" style="margin: 0 5px"></i></span>
+                  </td>
+                </tr>
+
+                </tbody>
+              </table>
+
+
+            </div>
+            <div class="card-footer">
+              <div class="card border">
+                  <button class="btn btn-outline-dark" @click="showModal = true">Ajouter une formation</button>
+
+              </div>
+            </div>
+          </div>
+      <!-- /.content -->
+        </div>
+      </div>
+
+      <div class="home_content">
+        <div v-if="showModal" v-cloak>
+          <transition name="modal">
+            <div class="modal-mask">
+              <div class="modal-wrapper">
+                <div class="modal-dialog modal-xl" role="document">
+                  <div class="modal-content" style="background-color: #e3fdfd ; border-radius: 15px ; border-color: black">
+                    <div class="modal-header" style="padding-bottom: 15px ; border-radius: 15px">
+                      <h5 class="modal-title">Entrer votre formation</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" @click="showModal = false">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+
+                      <div class="card" style="background-color: #e3fdfd ; border-radius: 15px ; border-color: black" >
+                        <div class="card-body">
+
+                          <div class="form-group">
+                            <label>Titre</label>
+                            <input type="text" style=" border-radius: 15px" class="form-control">
+                          </div>
+
+                          <div class="row">
+                            <div class="form-group col-md-4">
+                              <label>Nombre de jours de la formation</label>
+                              <input type="number" style=" border-radius: 15px" class="form-control">
+                            </div>
+
+                            <div class="form-group col-md-4">
+                              <label>Tarifs par jours</label>
+                              <input type="number" style=" border-radius: 15px" class="form-control">
+                            </div>
+
+                            <div class="form-group col-md-4">
+                              <label>Nombre de participants dans la formation</label>
+                              <input type="number" style=" border-radius: 15px" class="form-control">
+                            </div>
+                          </div>
+
+
+                          <div class="form-group">
+                            <label>Description</label>
+                            <textarea style="border-radius: 15px" class="form-control"/>
+                          </div>
+
+
+
+                        </div>
+                      </div>
+
+                    </div>
+                    <div class="modal-footer">
+                      <button class="btn btn-block btn-outline-success" style=" border-radius: 15px">Valider</button>
+                      <button class="btn btn btn-outline-secondary" style=" border-radius: 15px" @click="showModal = false">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </transition>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
+
+
 import MainSidebar from "@/layout/MainSidebar";
 
 export default {
   name: "Formations",
   components: {
     MainSidebar
+  },
+  data(){
+    return{
+      showModal: false
+    }
   }
 };
 </script>
@@ -54,4 +173,24 @@ export default {
   border: 2px solid #1c6ea4;
   border-radius: 10px;
 }
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+
+
 </style>
